cf52123b77c73ed2dbab516660cd23e2c4f12b77
Jeremiah <jeremiah@laynes.org>
2025-08-16 17:50:48 -0400

    Changed state to a byte array and set logic for making a blank board.

--- src/Board.java
+++ src/Board.java
@@ -1,22 +1,45 @@
+import static java.lang.Math.sqrt;
+
 /**
  * Basic class to represent the game state.
  */
 public class Board {
-    private int[][] state;
+    private byte[] state;
+    private int size;
 
-    // --- Initializer ---
-    public Board(int[][] newState) {
+    // --- Initializers ---
+    public Board(byte[] newState) {
         setState(newState);
+    }
+    public Board(int size) {
+        setSize(size);
+        setBlankState();
+    }
+    public Board() {
+        setSize(9);
+        setBlankState();
     }
 
     // --- Getters ---
-    public int[][] getState() {
+    public byte[] getState() {
         return state;
+    }
+    public int getSize() {
+        return size;
     }
 
     // --- Setters ---
-    public void setState(int[][] state) {
+    private void setBlankState(){
+        this.state = new byte[size];
+    }
+
+    public void setState(byte[] state) {
         this.state = state;
+    }
+    public void setSize(int size) {
+        if (0<size && size<128 && sqrt(size)%1==0) {
+            this.size = size;
+        }
     }
 
 }
